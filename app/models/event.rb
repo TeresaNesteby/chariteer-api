24df14b4c76f64bc9e82d04091f447067f3b54fc
Gai Vien & Teresa Nesteby <pair+gvien.teresanesteby@devbootcamp.com>
2015-08-29 12:15:32 -0700

    update model relationships

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ActiveRecord::Base
+  belongs_to :organization
+  belongs_to :volunteer
+  has_many :categories
 end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,7 @@
 class Organization < ActiveRecord::Base
+  has_secure_password
+  has_many :events
+  validates :contact_email, presence: true, uniqueness: true
+  validates :name, presence: true
+
 end
