84b809132b1a1b956a72d3603887e4189de2505b
Marisa Ramon <mer0522@gmail.com>
2015-08-31 21:34:48 -0700

    add token colummn to orgs & vols; update sessions & orgs controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,20 +6,26 @@ class SessionsController < ApplicationController
   end
 
   def create
-     if @volunteer = Volunteer.find_by(email: params[:volunteer][:email]).present?
-      if @volunteer.authenticate(params[:volunteer][:password])
+     volunteer = Volunteer.find_by(email: params[:volunteer][:email])
+     organization = Organization.find_by(email: params[:organization][:email])
+     if volunteer.present?
+      if volunteer.authenticate(params[:volunteer][:password])
       # Save the volunteer ID in the session so it can be used in
       # subsequent requests
-        session[:id] = @volunteer.id
-        redirect_to volunteer_path
+        session[:id] = volunteer.id
+        volunteer.token = "OK"
+        volunteer.save
+        render json: volunteer, status: 200
       end
 
-     elsif organization = Organization.find_by(email: params[:organization][:email]).present?
+     elsif organization.present?
       if organization.authenticate(params[:organization][:password])
       # Save the organization ID in the session so it can be used in
       # subsequent requests
         session[:id] = organization.id
-        redirect_to organization_path(organization)
+        organization.token = "OK"
+        organization.save
+        render json: organization, status: 200
       end
     else
       flash[:danger] = 'Invalid email/password combination'
@@ -29,6 +35,12 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:id] = nil
-    redirect_to root_path
+
+    if volunteer = Volunteer.find(params[:id])
+      volunteer.update(token: nil)
+    elsif organization = Organization.find(params[:id])
+      organization.update(token: nil)
+    end
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150831235721) do
+ActiveRecord::Schema.define(version: 20150901033031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20150831235721) do
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
     t.string   "logo_image"
+    t.string   "token"
   end
 
   create_table "participants", force: :cascade do |t|
@@ -65,6 +66,7 @@ ActiveRecord::Schema.define(version: 20150831235721) do
     t.datetime "updated_at",      null: false
     t.string   "interest"
     t.string   "photo_url"
+    t.string   "token"
   end
 
 end
