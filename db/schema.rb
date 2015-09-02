220646fbcd79794cd3fe52dd7e8af18c94870561
Gai Vien <wavetech86@gmail.com>
2015-09-02 00:29:28 -0700

    WIP, update routes for sessions & update sessions controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150901033031) do
+ActiveRecord::Schema.define(version: 20150901073828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "api_keys", force: :cascade do |t|
+    t.string   "access_token"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "category_type"
