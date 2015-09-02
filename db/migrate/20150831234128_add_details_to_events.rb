4e91d594096aa36c97f523dce130b514d485b122
gvien <wavetech86@gmail.com>
2015-09-02 16:12:32 -0700

    update routes, event migration, and seeds for the post ng end

--- db/migrate/20150831234128_add_details_to_events.rb
+++ db/migrate/20150831234128_add_details_to_events.rb
@@ -1,7 +1,7 @@
 class AddDetailsToEvents < ActiveRecord::Migration
   def change
     add_column :events, :org_name, :string
-    add_column :events, :location, :string
+    add_column :events, :place, :string
     add_column :events, :date, :string
     add_column :events, :time, :string
     add_column :events, :num_of_volunteers_needed, :integer
